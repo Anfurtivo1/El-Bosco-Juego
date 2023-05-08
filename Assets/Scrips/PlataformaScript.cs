47bec09a3873c17863b449f940432df50516d497
Andres <anfurtivo1@hotmail.com>
2023-05-08 14:54:26 +0200

    Arreglos interfaz modo historia
    Se han arreglado algunas cosas de la interfaz del modo historia

--- Assets/Scrips/PlataformaScript.cs
+++ Assets/Scrips/PlataformaScript.cs
@@ -16,4 +16,21 @@ public class PlataformaScript : MonoBehaviour
     {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("finishLine"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("finishLine"))
+    //    {
+    //        Destroy(this.gameObject);
+    //    }
+    //}
+
 }
